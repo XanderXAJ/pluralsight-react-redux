6fb2883ab032c44bc6fcd439ec786e0f991355aa
Alex Palmer <xanderx@xanderx.com>
2017-02-25 22:55:29 +0000

    Display notification when form is saved

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -3,6 +3,7 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import * as actions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
+import toastr from 'toastr';
 
 class ManageCoursePage extends React.Component {
   constructor(props, context) {
@@ -41,6 +42,7 @@ class ManageCoursePage extends React.Component {
 
   redirect() {
     this.setState({saving: false});
+    toastr.success('Course saved');
     this.context.router.push('/courses');
   }
 
