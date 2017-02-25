c3eb086264b58a7bd794dc6bd345f4be70bf4dc2
Alex Palmer <xanderx@xanderx.com>
2017-02-25 13:26:06 +0000

    Add redux-logger, do not apply middleware on production

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,17 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import reduxLogger from 'redux-logger';
+
+let middleware = [];
+if (process.env.NODE_ENV !== 'production') {
+  middleware = [...middleware, reduxImmutableStateInvariant(), reduxLogger()];
+}
 
 export default function configureStore(initialState) {
   return createStore(
       rootReducer,
       initialState,
-      applyMiddleware(reduxImmutableStateInvariant())
+      applyMiddleware(...middleware)
   );
 }
