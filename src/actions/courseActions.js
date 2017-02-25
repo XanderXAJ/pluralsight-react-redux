ffbca8ec04fc9a53cde87a86d1ebf798c4fc2f71
Alex Palmer <xanderx@xanderx.com>
2017-02-25 23:02:15 +0000

    Hide status indicator after AJAX failure

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,6 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
-import {beginAjaxCall} from './ajaxStatusActions';
+import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
 
 export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
@@ -22,6 +22,7 @@ export function loadCourses() {
           dispatch(loadCoursesSuccess(courses));
         }
     ).catch(error => {
+      dispatch(ajaxCallError());
       throw(error);
     });
   };
@@ -35,6 +36,7 @@ export function saveCourse(course) {
           dispatch(updateCourseSuccess(savedCourse)) :
           dispatch(createCourseSuccess(savedCourse));
     }).catch(error => {
+      dispatch(ajaxCallError());
       throw error;
     });
   };
