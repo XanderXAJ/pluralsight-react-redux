105d932f5eda970775b77f7ff3e5c51524b56eee
Alex Palmer <xanderx@xanderx.com>
2017-02-25 14:29:12 +0000

    Add redux-thunk middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,8 +2,9 @@ import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import reduxLogger from 'redux-logger';
+import thunk from 'redux-thunk';
 
-let middleware = [];
+let middleware = [thunk];
 if (process.env.NODE_ENV !== 'production') {
   middleware = [...middleware, reduxImmutableStateInvariant(), reduxLogger()];
 }
