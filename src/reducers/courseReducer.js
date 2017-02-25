0e1e8a2703694915192cef849b8e3a30997bbcd1
Alex Palmer <xanderx@xanderx.com>
2017-02-25 13:42:36 +0000

    Reduce correct action

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,6 +1,6 @@
 export default function courseReducer(state = [], action) {
   switch (action.type) {
-    case 'CREATE_SOURCE':
+    case 'CREATE_COURSE':
       return [
           ...state,
           Object.assign({}, action.course)
